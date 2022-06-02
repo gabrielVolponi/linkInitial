bc6e48a2f3bff4c780741ce178a2d373a3d40f3c
gabrielVolponi <gabriel.volponi@task-check.com>
2022-06-02 13:56:53 -0300

    first commit with application and test

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-"# linkInitial" 
+"# FormInitial" 
+
+Projeto de teste e2e em um formulário html e css simulando evento real na filosofia BDD.<br> <blockquote>Behavior Driven Development é uma técnica de desenvolvimento Ágil que encoraja colaboração entre desenvolvedores, setores de qualidade e pessoas não-técnicas ou de negócios num projeto de software, relaciona-se com o conceito de verificação e validação.
+
+Trata-se de um simples formulário em html com required para mostrar alguns casos de teste de fluxos principais e de excessão que podem seer usados hoje em dia.
+
+Versão 10.0.1 Cypress.io
+
+<h6><b>PARA INICIAR O PROJETO<b><h6>
+
+ <ul> 
+   <li>npm i (instalar todas dependências)  <br> 
+ <li>npx cypress open (abre o cypress em modo GUI interativo para rodar as specs separadamente) <br>
+ <li>npx cypress run (roda todas as specs em ordem alfabética no modo headless -> grava vídeo pasta cypress/videos)<br>
+ <li>npx cypress run --spec caminhoDaSpec/ (roda apenas a spec em modo headless) 
+
+<ul>
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,257 @@
+const faker = require('faker-br');
+
+describe('Form Basic filled', () => {
+  beforeEach('', () => {
+    cy.visit('../../dev/index.html')
+
+    //validação Gui
+    //titulo e subtitulo
+    cy.get('[data-cy="tituloLogin"]').contains('Login Sistema Tester');
+    cy.get('[data-cy="subtituloLogin"]').contains('Faça login com suas credenciais');
+
+    //css e html do formulário
+    cy.get('[data-cy="nomeTexto"]').contains('Nome');
+    cy.get('[data-cy="sobrenomeTexto"]').contains('Sobrenome');
+    cy.get('[data-cy="emailTexto"]').contains('Email');
+    cy.get('[data-cy="perguntaLinguagens"]').contains('De qual lado da aplicação você automatiza?');
+    cy.get('[data-cy="textoCypress"]').contains('Cypress.io');
+    cy.get('[data-cy="textoAppium"]').contains('Appium');
+    cy.get('[data-cy="textoSelenium"]').contains('Selenium');
+    cy.get('[data-cy="Fullstack"]').contains('Fullstack');
+
+    //validação dos inputs
+    cy.get('[data-cy="inputNome"]').should('be.empty');
+    cy.get('[data-cy="inputSobreNome"]').should('be.empty');
+    cy.get('[data-cy="inputEmail"]').should('be.empty');
+
+    cy.get('[data-cy="radioAppium"]').should('not.be.checked');
+    cy.get('[data-cy="radioSelenium"]').should('not.be.checked');
+    cy.get('[data-cy="radioFullstack"]').should('not.be.checked');
+
+    //valdiação button
+    cy.get('[data-cy="botaoConcluir"]').should('be.enabled');
+
+  });
+
+  //fluxos principais
+  it('Cadastro válido Cypress.io', () => {
+    let randomName = faker.name.firstName();
+    let randomLastName = faker.name.lastName();
+    let randomEmail = faker.internet.email();
+    cy.get('[data-cy="inputNome"]').type(randomName, { delay: 0 });
+    cy.get('[data-cy="inputNome"]').should('have.value', randomName);
+
+    cy.get('[data-cy="inputSobreNome"]').type(randomLastName, { delay: 0 });
+    cy.get('[data-cy="inputSobreNome"]').should('have.value', randomLastName);
+
+    cy.get('[data-cy="inputEmail"]').type(randomEmail, { delay: 0 });
+    cy.get('[data-cy="inputEmail"]').should('have.value', randomEmail);
+
+    cy.wait(2000);
+
+    //antes de enviar
+    cy.screenshot('antesDeEnviar-Cypress');
+
+    //act
+    cy.get('[data-cy="botaoConcluir"]').click();
+
+
+    //deepois de denviar
+    cy.screenshot('depoisDeEnviar-Cypress');
+
+  });
+
+  it('Cadastro válido Appium', () => {
+    let randomName = faker.name.firstName();
+    let randomLastName = faker.name.lastName();
+    let randomEmail = faker.internet.email();
+    cy.get('[data-cy="inputNome"]').type(randomName, { delay: 0 });
+    cy.get('[data-cy="inputNome"]').should('have.value', randomName);
+
+    cy.get('[data-cy="inputSobreNome"]').type(randomLastName, { delay: 0 });
+    cy.get('[data-cy="inputSobreNome"]').should('have.value', randomLastName);
+
+    cy.get('[data-cy="inputEmail"]').type(randomEmail, { delay: 0 });
+    cy.get('[data-cy="inputEmail"]').should('have.value', randomEmail);
+
+    cy.get('[data-cy="radioAppium"]').check();
+
+    cy.wait(2000);
+
+    //antes de enviar
+    cy.screenshot('antesDeEnviar-Appium');
+
+    //act
+    cy.get('[data-cy="botaoConcluir"]').click();
+
+
+    //deepois de denviar
+    cy.screenshot('depoisDeEnviar-Appium');
+
+  });
+
+  it('Cadastro válido Selenium', () => {
+    let randomName = faker.name.firstName();
+    let randomLastName = faker.name.lastName();
+    let randomEmail = faker.internet.email();
+    cy.get('[data-cy="inputNome"]').type(randomName, { delay: 0 });
+    cy.get('[data-cy="inputNome"]').should('have.value', randomName);
+
+    cy.get('[data-cy="inputSobreNome"]').type(randomLastName, { delay: 0 });
+    cy.get('[data-cy="inputSobreNome"]').should('have.value', randomLastName);
+
+    cy.get('[data-cy="inputEmail"]').type(randomEmail, { delay: 0 });
+    cy.get('[data-cy="inputEmail"]').should('have.value', randomEmail);
+
+    cy.get('[data-cy="radioSelenium"]').check();
+
+    cy.wait(2000);
+
+    //antes de enviar
+    cy.screenshot('antesDeEnviar-Selenium');
+
+    //act
+    cy.get('[data-cy="botaoConcluir"]').click();
+
+
+    //deepois de denviar
+    cy.screenshot('depoisDeEnviar-Selenium');
+
+  });
+
+  it('Cadastro válido Fullstack', () => {
+    let randomName = faker.name.firstName();
+    let randomLastName = faker.name.lastName();
+    let randomEmail = faker.internet.email();
+    cy.get('[data-cy="inputNome"]').type(randomName, { delay: 0 });
+    cy.get('[data-cy="inputNome"]').should('have.value', randomName);
+
+    cy.get('[data-cy="inputSobreNome"]').type(randomLastName, { delay: 0 });
+    cy.get('[data-cy="inputSobreNome"]').should('have.value', randomLastName);
+
+    cy.get('[data-cy="inputEmail"]').type(randomEmail, { delay: 0 });
+    cy.get('[data-cy="inputEmail"]').should('have.value', randomEmail);
+
+    cy.get('[data-cy="radioFullstack"]').check();
+
+    cy.wait(2000);
+
+    //antes de enviar
+    cy.screenshot('antesDeEnviar-Fullstack');
+
+    //act
+    cy.get('[data-cy="botaoConcluir"]').click();
+
+
+    //deepois de denviar
+    cy.screenshot('depoisDeEnviar-Fullstack');
+
+  });
+
+  //fluxos excessão
+
+  it('Nome null', () => {
+    let randomLastName = faker.name.lastName();
+    let randomEmail = faker.internet.email();
+
+    cy.get('[data-cy="inputNome"]').should('have.value', '');
+
+    cy.get('[data-cy="inputSobreNome"]').type(randomLastName, { delay: 0 });
+    cy.get('[data-cy="inputSobreNome"]').should('have.value', randomLastName);
+
+    cy.get('[data-cy="inputEmail"]').type(randomEmail, { delay: 0 });
+    cy.get('[data-cy="inputEmail"]').should('have.value', randomEmail);
+
+    cy.wait(2000);
+
+    //antes de enviar
+    cy.screenshot('antesDeEnviar-nome-null');
+
+    //act
+    cy.get('[data-cy="botaoConcluir"]').click();
+
+
+    //deepois de denviar
+    cy.screenshot('depoisDeEnviar-nome-null');
+
+  });
+
+  it('Sobrenome Null', () => {
+    let randomName = faker.name.firstName();
+    let randomEmail = faker.internet.email();
+    cy.get('[data-cy="inputNome"]').type(randomName, { delay: 0 });
+    cy.get('[data-cy="inputNome"]').should('have.value', randomName);
+
+    cy.get('[data-cy="inputSobreNome"]').should('have.value', '');
+
+    cy.get('[data-cy="inputEmail"]').type(randomEmail, { delay: 0 });
+    cy.get('[data-cy="inputEmail"]').should('have.value', randomEmail);
+
+
+    cy.wait(2000);
+
+    //antes de enviar
+    cy.screenshot('antesDeEnviar-sobrenome-null');
+
+    //act
+    cy.get('[data-cy="botaoConcluir"]').click();
+
+
+    //deepois de denviar
+    cy.screenshot('depoisDeEnviar-sobrenome-null');
+
+  });
+
+  it('Email null', () => {
+    let randomName = faker.name.firstName();
+    let randomLastName = faker.name.lastName();
+
+    cy.get('[data-cy="inputNome"]').type(randomName, { delay: 0 });
+    cy.get('[data-cy="inputNome"]').should('have.value', randomName);
+
+    cy.get('[data-cy="inputSobreNome"]').type(randomLastName, { delay: 0 });
+    cy.get('[data-cy="inputSobreNome"]').should('have.value', randomLastName);
+
+    cy.get('[data-cy="inputEmail"]').should('have.value', '');
+
+    cy.wait(2000);
+
+    //antes de enviar
+    cy.screenshot('antesDeEnviar-email-null');
+
+    //act
+    cy.get('[data-cy="botaoConcluir"]').click();
+
+
+    //deepois de denviar
+    cy.screenshot('depoisDeEnviar-email-null');
+
+  });
+
+  it('Todos dados null', () => {
+
+    cy.get('[data-cy="inputNome"]').should('have.value', '');
+
+    cy.get('[data-cy="inputSobreNome"]').should('have.value', '');
+
+    cy.get('[data-cy="inputEmail"]').should('have.value', '');
+
+    cy.wait(2000);
+
+    //antes de enviar
+    cy.screenshot('antesDeEnviar-tudo-null');
+
+    //act
+    cy.get('[data-cy="botaoConcluir"]').click();
+
+
+    //deepois de denviar
+    cy.screenshot('depoisDeEnviar-tudo-null');
+
+  });
+
+
+
+
+
+});
